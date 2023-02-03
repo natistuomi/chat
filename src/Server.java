2427c1a252ee5551d583f332f32711688f28a005
natistuomi <77719270+natistuomi@users.noreply.github.com>
2023-02-03 12:58:29 +0100

    Initial commit

---
+++ src/Server.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+    ServerSocket server;
+    Socket client;
+
+    PrintWriter out;
+    BufferedReader in;
+
+    public Server(int port) {
+        try {
+            server = new ServerSocket(port);
+        } catch (IOException e) {
+            System.err.println("Failed to open serversocket.");
+            e.printStackTrace();
+        }
+        System.out.println("Server started...");
+    }
+
+    private void acceptClient() {
+        try {
+            client = server.accept();
+        } catch (IOException e) {
+            System.err.println("Failed to connect to client");
+            e.printStackTrace();
+        }
+        System.out.println("client connected...");
+    }
+
+    private void getStreams() {
+        try {
+            out = new PrintWriter(client.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Streams ready...");
+    }
+
+    private void runProtocol() {
+        Scanner tgb = new Scanner(System.in);
+        System.out.println("chatting...");
+        String msg = "";
+        while (!msg.equals("QUIT")) {
+            msg = tgb.nextLine();
+            out.println("SERVER: " + msg);
+        }
+    }
+
+    public static void main(String[] args) {
+        Server s = new Server(1234);
+        s.acceptClient();
+        s.getStreams();
+        ListenerThread l = new ListenerThread(s.in, System.out);
+        Thread listener = new Thread(l);
+        listener.start();
+        s.runProtocol();
+        listener.stop();
+        s.shutdown();
+    }
+
+    private void shutdown() {
+        try {
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
