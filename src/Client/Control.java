5e04c747e9a3f0f98aa7ded53278d1d51473fed8
natistuomi <77719270+natistuomi@users.noreply.github.com>
2023-02-16 12:49:44 +0100

    view-delen-av-genomgången

--- src/Client/Control.java
+++ src/Client/Control.java
@@ -1,7 +1,27 @@
 package Client;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Control {
     static Model model = new Model("10.70.45.159", 1234) ;
+    static View view = new View();
+
+    public Control() {
+        view.getButton().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        JFrame frame = new JFrame("View");
+        frame.setContentPane(view.getPane());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 
     public static void main(String[] args) throws InterruptedException {
         model.getStreams();
--- src/Client/View.java
+++ src/Client/View.java
@@ -1,10 +1,22 @@
 package Client;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class View {
     private JTextArea chatarea;
     private JPanel pane;
     private JTextField inputfield;
     private JButton button;
+
+    public JButton getButton() {
+        return button;
+    }
+
+    public JPanel getPane() {
+        return pane;
+    }
+
+
 }
